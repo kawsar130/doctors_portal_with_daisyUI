3444b8befb4fd08bd6fb507ad870ec067e62b856
Kawsar Ahmed <kawsarahmed130@gmail.com>
2022-08-14 15:19:06 +0600

    Booking Modal Added

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Service = ({ service }) => {
+const Service = ({ service, setTreatment }) => {
   const { name, slots } = service;
   return (
     <div className="card lg:max-w-lg bg-base-100 shadow-xl">
@@ -17,12 +17,14 @@ const Service = ({ service }) => {
           {slots.length} {slots.length > 1 ? "spaces" : "space"} available
         </p>
         <div className="card-actions justify-center">
-          <button
+          <label
             disabled={slots.length === 0}
-            className="btn btn-secondary text-white uppercase"
+            for="booking-modal"
+            onClick={() => setTreatment(service)}
+            class="btn btn-secondary text-white uppercase"
           >
             Book Appointment
-          </button>
+          </label>
         </div>
       </div>
     </div>
