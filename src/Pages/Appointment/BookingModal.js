5ba2a1a73e6e09a2dd979e236afe7843d5d1683a
Kawsar Ahmed <kawsarahmed130@gmail.com>
2022-08-21 18:09:47 +0600

    Booking Modal updated

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -6,11 +6,21 @@ import auth from "../../firebase.init";
 const BookingModal = ({ treatment, date, setTreatment }) => {
   const { _id, name, slots } = treatment;
   const [user, loading, error] = useAuthState(auth);
+  const formattedDate = format(date, "PP");
 
   const handleBooking = (event) => {
     event.preventDefault();
     const slot = event.target.slot.value;
     console.log(_id, name, slots);
+    const booking = {
+      treatmentId: _id,
+      treatment: name,
+      date: formattedDate,
+      slot,
+      patient: user.email,
+      patientName: user.displayName,
+      phone: event.target.phone.value,
+    };
 
     // to close the modal
     setTreatment(null);
