d53272e636e3e1d43e5460ab4c6a1127bddaafc2
Kawsar Ahmed <kawsarahmed130@gmail.com>
2022-08-29 23:59:19 +0600

    test

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -2,8 +2,29 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h2>Dash Dash Dashing Board</h2>
+    <div class="drawer drawer-mobile">
+      <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content flex flex-col items-center justify-center">
+        {/* <!-- Page content here --> */}
+        <label
+          for="my-drawer-2"
+          class="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div class="drawer-side">
+        <label for="my-drawer-2" class="drawer-overlay"></label>
+        <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+          {/* <!-- Sidebar content here --> */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -28,6 +28,11 @@ const Navbar = () => {
       <li>
         <Link to="/contact">Contact Us</Link>
       </li>
+      {user && (
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      )}
       <li>
         {user ? (
           <button onClick={logout} className="btn btn-ghost">
